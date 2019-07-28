10c92a1d625b9c620ad608b9fa5c301a8452388d
Paulo Henrique <phmelomorais@gmail.com>
2019-07-28 18:23:26 -0300

    Configuring  Styled components

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
-import { Title } from './styles';
+
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './styles';
 
 function Main() {
-  return <Title error={false}>Hello World</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+        <SubmitButton disabled>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 export default Main;
